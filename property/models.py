0a1442b131f336507218869981571875a506265b
kodunaiff <kodunaiff@gmail.com>
2023-12-13 04:01:54 +0800

    add Owner model

--- property/models.py
+++ property/models.py
@@ -74,3 +74,15 @@ class Complaint(models.Model):
 
     def __str__(self):
         return f'{self.flat} {self.text}'
+
+
+class Owner(models.Model):
+    name = models.CharField('ФИО владельца:', max_length=200)
+    phonenumber = models.CharField('Номер владельца:', max_length=20)
+    pure_phone = PhoneNumberField(
+        'Нормализованный номер владельца',
+        blank=True, max_length=20)
+    flats = models.ManyToManyField(Flat, verbose_name='Квартиры в собственности:', related_name='owners')
+
+    def __str__(self):
+        return self.name
