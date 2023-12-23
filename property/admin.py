408e2a6a726f9a46aa74de3ef74b56a5ed8976f8
kodunaiff <kodunaiff@gmail.com>
2023-12-23 19:12:34 +0800

    add decorators

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class OwnerInline(admin.TabularInline):
     raw_id_fields = ['owner']
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address',)
     readonly_fields = ['created_at']
@@ -24,14 +25,11 @@ class FlatAdmin(admin.ModelAdmin):
     ]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat')
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
